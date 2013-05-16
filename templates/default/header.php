2e23459a0c2a2505f20160a1fb9d1832f1a65097
Juan Mendoza <mendozajuan007@gmail.com>
2013-05-16 13:16:59 -0430

    nuevo diseno

--- templates/default/header.php
+++ templates/default/header.php
@@ -1,10 +1,40 @@
-<!doctype html>
+<!DOCTYPE html>
 <?php Session::init(); ?>
 
-<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=7">
+<html content="text/html; charset=UTF-8">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=7">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-                <title>M & R Valle Hondo</title>
+    <title>M & R Valle Hondo</title>
+
+    <link href="<?php echo URL; ?>public/css/bootstrap.css" rel="stylesheet">
+    <link href="<?php echo URL; ?>public/css/default.css" rel="stylesheet">
+    <link href="<?php echo URL; ?>public/css/font-awesome.min.css" rel="stylesheet">
+    <link href="<?php echo URL; ?>public/css/font-awesome-ie7.min.css" rel="stylesheet">
+    <link href="<?php echo URL; ?>public/css/bootstrap-responsive.css" rel="stylesheet">
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+        <script src="js/html5shiv.js"></script>
+    <![endif]-->
+    <script src="<?php echo PATH_FILE; ?>public/js/jquery.js"></script>
+    <script src="<?php echo PATH_FILE; ?>public/js/bootstrap-transition.js"></script>
+    <script src="<?php echo PATH_FILE; ?>public/js/bootstrap-alert.js"></script>
+    <script src="<?php echo PATH_FILE; ?>public/js/bootstrap-modal.js"></script>
+    <script src="<?php echo PATH_FILE; ?>public/js/bootstrap-dropdown.js"></script>
+    <script src="<?php echo PATH_FILE; ?>public/js/bootstrap-scrollspy.js"></script>
+    <script src="<?php echo PATH_FILE; ?>public/js/bootstrap-tab.js"></script>
+    <script src="<?php echo PATH_FILE; ?>public/js/bootstrap-tooltip.js"></script>
+    <script src="<?php echo PATH_FILE; ?>public/js/bootstrap-popover.js"></script>
+    <script src="<?php echo PATH_FILE; ?>public/js/bootstrap-button.js"></script>
+    <script src="<?php echo PATH_FILE; ?>public/js/bootstrap-collapse.js"></script>
+    <script src="<?php echo PATH_FILE; ?>public/js/bootstrap-carousel.js"></script>
+    <script src="<?php echo PATH_FILE; ?>public/js/bootstrap-typeahead.js"></script>
+    <script src="<?php echo PATH_FILE; ?>public/js/bootstrap-tab.js"></script>
+    <script src="<?php echo PATH_FILE; ?>public/js/custom.js"></script>
                 <!--<style type="text/css" media="all">
                     @import url("<?php echo URL; ?>public/css/style.css");
                     @import url("<?php echo URL; ?>public/css/jquery.wysiwyg.css");
@@ -13,7 +43,7 @@
                     @import url("<?php echo URL; ?>public/css/date_input.css");
                     @import url("<?php echo URL; ?>public/jquery-ui/css/smoothness/jquery-ui-1.8.20.custom.css");
                 </style>-->
-                <script type="text/javascript" src="<?php echo URL; ?>public/js/jquery.js"></script>
+                <!--<script type="text/javascript" src="<?php echo URL; ?>public/js/jquery.js"></script>
                 <script type="text/javascript" src="<?php echo URL; ?>public/js/jquery.img.preload.js"></script>
                 <script type="text/javascript" src="<?php echo URL; ?>public/js/jquery.filestyle.mini.js"></script>
                 <script type="text/javascript" src="<?php echo URL; ?>public/js/jquery.wysiwyg.js"></script>
@@ -28,64 +58,112 @@
                 <script type="text/javascript" src="<?php echo URL; ?>public/js/jquery.tipsy.js"></script>
                 <script type="text/javascript" src="<?php echo URL; ?>public/js/jquery.validate.js"></script>
                 <script type="text/javascript" src="<?php echo URL; ?>public/js/custom.js"></script>
-                <script type="text/javascript" src="<?php echo URL; ?>public/jquery-ui/js/jquery-ui-1.8.20.custom.min.js"></script>
+                <script type="text/javascript" src="<?php echo URL; ?>public/jquery-ui/js/jquery-ui-1.8.20.custom.min.js"></script>-->
                
-                 <?php $this->mostrarJs(); ?>
-                </head>
-                <body>
-                    <div id="hld">
-                        <div class="wrapper">
-                            <div id="header">
-                                <div class="hdrl"></div>
-                                <div class="hdrr"></div>
-                                <h1><a href="#">M & R Valle Hondo</a></h1>
-                                <ul id="nav">
-                                    <li class="">
-                                        <a href="">Inicio</a>
-                                        <ul>
-                                            <li><a href="<?php echo URL; ?>">Inicio</a></li>
-                                            <?php if(Session::get('rol') == 'administrador'){?>
-                                            <li><a href="<?php echo URL; ?>usuario">Usuarios</a></li>
-                                            <?php }?>
-                                        </ul>
-                                    </li>
-                                    <li class="">
-                                        <a href="">Configuracion</a>
-                                        <ul>
-                                            <li><?php if(Session::get('rol') == 'administrador'){?>
-                                                <a href="<?php echo URL; ?>categoria">Definir Categorias</a></li>
-                                            <li><a href="<?php echo URL; ?>marca">Definir Marcas</a></li>
-                                            <li><a href="<?php echo URL; ?>muestra">Definir Cilindrada</a></li>
-                                             <?php }?>
-                                        </ul>
-                                    </li>
-                                    <li class="">
-                                        <a href="">Datos</a>
-                                        <ul>
-                                            <li><a href="<?php echo URL; ?>cliente">Clientes</a></li>
-                                            <li><a href="<?php echo URL; ?>proveedor">Proveedores</a></li>
-                                            <li><a href="<?php echo URL; ?>producto">Productos</a></li>
-                                        </ul>
-                                    </li>
+ <?php $this->mostrarJs(); ?>
+</head>
+<body>
+</head>
 
-                                    <li class="">
-                                        <a href="">Reporte General</a>
-                                        <ul>
-                                            <li><a href="<?php echo URL ?>producto/imprimir/">Generar PDF</a></li>
-                                            
-                                        </ul>
-                                    </li>
-
-                                                                     
-                                   
-                                </ul>
-
-                                <p class="user">Bienvenido, <a href="<?php echo URL; ?>usuario"><?php echo $this->usuario; ?></a> | <a href="<?php echo URL; ?>dashboard/logout">Salir</a></p>
-
-                            </div>
+<body>
+  <div class="navbar navbar-fixed-top">
+    <div class="navbar-inner">
+      <div class="container">
+        <div class="btn-group pull-right">
+          <div class="btn btn-warning disabled"><i class="icon-user icon-white"></i> <span class="hidden-phone">Ingresar</span></div>
+          <button class="btn btn-warning dropdown-toggle" data-toggle="dropdown">
+            <span class="caret"></span>
+          </button>
+          <ul class="dropdown-menu">
+            <li><a href="#"><i class="icon-pencil"></i> Editar Perfil</a></li>
+            <li class="divider"></li>
+            <li><a href="#"><i class="icon-off"></i> Salir</a></li>
+          </ul>
+        </div>
+        <div class ="ajaxLoader btn-group pull-right"><img class="hidden" src="<?php echo PATH_NAV; ?>public/images/ajax-loader.gif"></div>
+        <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="brand linkAjax" href="<?php echo PATH_NAV; ?>">SIPGOB</a>
+        <div class="nav-collapse collapse">
+          <nav>
+            <ul class="nav">
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Ordenes <b class="caret"></b></a>
+                <span style="position: absolute; top: 2px; font-size: 9px; padding: 1px 5px; right: 3px;" class="badge badge-success">1</span>
+                <ul class="dropdown-menu">
+                  <li><a class="linkAjax" href="<?php print PATH_NAV.'orden/admin'; ?>">Administrar</a></li>
+                  <li><a class="linkAjax" href="<?php print PATH_NAV.'orden/ver'; ?>">Ver</a></li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+  </div>
+  <div id="contentDATA" class="container">
 
 
-                            <div class="block">
+
+
+
+    
+    <!-- <div id="hld">
+        <div class="wrapper">
+            <div id="header">
+                <div class="hdrl"></div>
+                <div class="hdrr"></div>
+                <h1><a href="#">M & R Valle Hondo</a></h1>
+                <ul id="nav">
+                    <li class="">
+                        <a href="">Inicio</a>
+                        <ul>
+                            <li><a href="<?php echo URL; ?>">Inicio</a></li>
+                            <?php if(Session::get('rol') == 'administrador'){?>
+                            <li><a href="<?php echo URL; ?>usuario">Usuarios</a></li>
+                            <?php }?>
+                        </ul>
+                    </li>
+                    <li class="">
+                        <a href="">Configuracion</a>
+                        <ul>
+                            <li><?php if(Session::get('rol') == 'administrador'){?>
+                                <a href="<?php echo URL; ?>categoria">Definir Categorias</a></li>
+                            <li><a href="<?php echo URL; ?>marca">Definir Marcas</a></li>
+                            <li><a href="<?php echo URL; ?>muestra">Definir Cilindrada</a></li>
+                             <?php }?>
+                        </ul>
+                    </li>
+                    <li class="">
+                        <a href="">Datos</a>
+                        <ul>
+                            <li><a href="<?php echo URL; ?>cliente">Clientes</a></li>
+                            <li><a href="<?php echo URL; ?>proveedor">Proveedores</a></li>
+                            <li><a href="<?php echo URL; ?>producto">Productos</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="">
+                        <a href="">Reporte General</a>
+                        <ul>
+                            <li><a href="<?php echo URL ?>producto/imprimir/">Generar PDF</a></li>
+                            
+                        </ul>
+                    </li>
+
+                                                     
+                   
+                </ul>
+
+                <p class="user">Bienvenido, <a href="<?php echo URL; ?>usuario"><?php echo $this->usuario; ?></a> | <a href="<?php echo URL; ?>dashboard/logout">Salir</a></p>
+
+            </div>
+
+
+            <div class="block"> -->
                                 
 
 
